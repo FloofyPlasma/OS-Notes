dfe547a04c24a63ad58d8ee4c3a926a4c4ee7f71
FloofyPlasma <github@floofyplasma.com>
2024-12-23 00:17:25 -0500

    Decided to not do a custom domain.

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "os-notes.floofyplasma.com",
+    baseUrl: "floofyplasma.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
